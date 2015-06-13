f237fe55e5b067cadcc42c8583a965e2bafce41b
Dean Merchant <deanmerchant@gmail.com>
2015-06-13 12:56:56 +0100

    Added react-toastr, added css to index.

--- gulp_tasks/assets.js
+++ gulp_tasks/assets.js
@@ -3,11 +3,14 @@ var config = require('../config');
 var path = require('path');
 
 gulp.task('assets', function() {
+  // TODO concat these as in styles.js
   gulp
     .src([
       path.join(config.nodeModules, 'bootstrap', 'dist', 'css', 'bootstrap.css'),
       path.join(config.nodeModules, 'bootstrap', 'dist', 'css', 'bootstrap.css.map'),
-      path.join(config.nodeModules, 'font-awesome', 'css', 'font-awesome.css')
+      path.join(config.nodeModules, 'font-awesome', 'css', 'font-awesome.css'),
+      path.join(config.nodeModules, 'animate.css', 'animate.css'),
+      path.join(config.nodeModules, 'toastr', 'toastr.css')
     ])
     .pipe(gulp.dest(path.join(config.dest, 'vendor')));
 
